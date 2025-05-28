ef87588f442c428a02efab14133ed3403f9326df
Kalininaaw16012002 <kalinina_asya_1601@mail.ru>
2025-05-28 12:38:27 +0300

    Сделала доработки по 2 и 3 дз

--- hwjs/hw2.js
+++ hwjs/hw2.js
@@ -6,19 +6,19 @@ alert(a);
 
 //2 задание
 let yearOfRelease = 2007;
-alert (`год создания первого айфона ` + yearOfRelease);
+alert ('год создания первого айфона ' + yearOfRelease);
 
 //3 задание
-let creator = `Брендан Эйх`
-alert (`Создатель JavaScript ` + creator);
+let creator = 'Брендан Эйх'
+alert ('Создатель JavaScript ' + creator);
 
 //4 задание
 let b = Number(10);
 let c = Number(2);
-alert (`Сумма ` + (c+b));
-alert (`Разность ` +  (b-c));
-alert (`Произведение ` + (b*c));
-alert (`Частное ` + (b/c)); 
+alert ('Сумма ' + (c+b));
+alert ('Разность ' +  (b-c));
+alert ('Произведение ' + (b*c));
+alert ('Частное ' + (b/c)); 
 
 //5 задание
 let result = 2**5;
@@ -40,7 +40,7 @@ num -=1;
 alert(num);
 
 //8 задание
-let age = prompt(`Cколько вам лет?`);
+let age = prompt('Cколько вам лет?');
 alert(age);
 
 //9 задание
@@ -52,11 +52,11 @@ const user = {
 console.log(user.name);
 
 //10 задание
-let name = prompt(`Назовите ваше имя:`);
-alert (`Привет, ` + name);
+let name = prompt('Назовите ваше имя:');
+alert ('Привет, ' + name);
 
 //Доп задание
-let number= prompt(`Назовите любое число:`);
+let number= prompt('Назовите любое число:');
 numberNew1 = number * 2;
 alert (numberNew1);
 numberNew2 = numberNew1 +10;
@@ -65,4 +65,4 @@ numberNew3 = numberNew2 / 2;
 alert (numberNew3);
 numberNew4 = numberNew3 - number;
 alert (numberNew4);
-alert (`Ответ: ` + 5);
+alert ('Ответ: ' + 5);
--- hwjs/hw3.js
+++ hwjs/hw3.js
@@ -1,23 +1,23 @@
 // 1 задание
 let password = 'пароль';
-let userPassword = prompt(`Введите ваш пароль: `);
+let userPassword = prompt('Введите ваш пароль: ');
 
 if (password === userPassword) {
-    console.log(`Пароль введен верно`);
+    console.log('Пароль введен верно');
 } else {
-    console.log(`Пароль введен неправильно`);
+    console.log('Пароль введен неправильно');
 }
 
 // 2 задание
-let c = Number(prompt(`Введите число: `));
+let c = Number(prompt('Введите число: '));
 
-(c>0 && c<10) ? console.log(`верно`) : console.log(`неверно`);
+(c>0 && c<10) ? console.log('верно') : console.log('неверно');
 
 // 3 задание
-let d = Number(prompt(`Введите 1 число: `));
-let e = Number(prompt(`Введите 2 число: `));
+let d = Number(prompt('Введите 1 число: '));
+let e = Number(prompt('Введите 2 число: '));
 
-(d>100 || e>100) ? console.log(`верно`) : console.log(`неверно`);
+(d>100 || e>100) ? console.log('верно') : console.log('неверно');
 
 // 4 задание
 let a = '2';
@@ -28,52 +28,52 @@ b = Number(b);
 alert(a+b);
 
 // 5 задание
-let monthNumber = Number(prompt(`Введите месяц: `));
+let monthNumber = Number(prompt('Введите месяц: '));
 
 switch (monthNumber) {
     case 1:
-        console.log(`Зима`);
+        console.log('Зима');
         break;
     case 2:
-        console.log(`Зима`);
+        console.log('Зима');
         break;
     case 3:
-        console.log(`Весна`);
+        console.log('Весна');
         break;
     case 4:
-        console.log(`Весна`);
+        console.log('Весна');
         break;
     case 5:
-        console.log(`Весна`);
+        console.log('Весна');
         break;
     case 6:
-        console.log(`Лето`);
+        console.log('Лето');
         break;
     case 7:
-        console.log(`Лето`);
+        console.log('Лето');
         break;
     case 8:
-        console.log(`Лето`);
+        console.log('Лето');
         break;
     case 9:
-        console.log(`Осень`);
+        console.log('Осень');
         break;
     case 10:
-        console.log(`Осень`);
+        console.log('Осень');
         break;
     case 11:
-        console.log(`Осень`);
+        console.log('Осень');
         break;
     case 12:
-        console.log(`Зима`);
+        console.log('Зима');
         break;
     default:
-        console.log(`Введено что-то не то`);
+        console.log('Введено что-то не то');
         break;
 }
 
 // 1 доп задание
-let number = prompt(`Пожалуйста, введите любое число`);
+let number = prompt('Пожалуйста, введите любое число');
 
 if (isNaN(number)) {
   console.log('Нужно писать число!');
@@ -86,28 +86,28 @@ if (isNaN(number)) {
 }
 
 // 2 доп задание
-let clientOS1 = Number(prompt(`Пожалуйста, введите 0 или 1`));
+let clientOS1 = Number(prompt('Пожалуйста, введите 0 или 1'));
 
 if (clientOS1 === 0) {
-    console.log(`Установите версию приложения для iOS по ссылке`);
+    console.log('Установите версию приложения для iOS по ссылке');
 } else  if (clientOS1 === 1) {
-    console.log(`Установите версию приложения для Android по ссылке`);
+    console.log('Установите версию приложения для Android по ссылке');
 } else {
-    console.log(`Некорректный ввод`);
+    console.log('Некорректный ввод');
 }   
 
 // 3 доп задание
-let clientOS2 = Number(prompt(`Пожалуйста, введите 0 или 1`));
-let phoneYear = Number(prompt(`Пожалуйста, введите год производства телефона`));
+let clientOS2 = Number(prompt('Пожалуйста, введите 0 или 1'));
+let phoneYear = Number(prompt('Пожалуйста, введите год производства телефона'));
 
 if (clientOS2 === 0 && phoneYear >= 2015 ) {
-    console.log(`Установите версию приложения для iOS по ссылке`);
+    console.log('Установите версию приложения для iOS по ссылке');
 } else  if (clientOS2 === 0 && phoneYear < 2015) {
-    console.log(`Установите облегченную версию приложения для iOS по ссылке`);
+    console.log('Установите облегченную версию приложения для iOS по ссылке');
 } else  if (clientOS2 === 1 && phoneYear >= 2015) {
-    console.log(`Установите версию приложения для Android по ссылке`);
+    console.log('Установите версию приложения для Android по ссылке');
 } else  if (clientOS2 === 1 && phoneYear < 2015) {
-    console.log(`Установите облегченную версию приложения для Android по ссылке`);
+    console.log('Установите облегченную версию приложения для Android по ссылке');
 } else {
-    console.log(`Некорректный ввод`);
+    console.log('Некорректный ввод');
 }   
