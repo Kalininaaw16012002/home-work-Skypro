b8e9284ea64094bd953b7a7dd21d0c6de4b43711
Kalininaaw16012002 <kalinina_asya_1601@mail.ru>
2025-06-12 22:21:44 +0300

    Начала 6 дз

--- script.js
+++ script.js
@@ -4,19 +4,42 @@ function guessTheNumber() {
 
     while(true){
         let userNumber = Number(prompt('Угадайте случайное число от 1 до 100'));
-            if (userNumber === 0) {
-                return; 
-            }
-
-            if (userNumber > randomNumber) {
-                alert('Ваше число больше загаднного, попробуйте еще');
-            } else if (userNumber < randomNumber) {
-                alert('Ваше число меньше загаданного, попробуйте еще');
-            } else if (userNumber === randomNumber){
-                alert('Поздравляем! Вы угадали! Загаданное число:' + randomNumber);
-                break;
-            } else {
-                alert('Ввели что-то не то, попробуйте снова');
-            }  
+        if (userNumber === 0) {
+            return; 
         }
+
+        if (userNumber > randomNumber) {
+            alert('Ваше число больше загаднного, попробуйте еще');
+        } else if (userNumber < randomNumber) {
+            alert('Ваше число меньше загаданного, попробуйте еще');
+        } else if (userNumber === randomNumber){
+            alert('Поздравляем! Вы угадали! Загаданное число:' + randomNumber);
+            break;
+        } else {
+            alert('Ввели что-то не то, попробуйте снова');
+        }  
+    }
+}
+
+function simpleArithmetic() {
+    let simpleArithmeticNumber = Math.ceil(Math.random()*100);
+    console.log(simpleArithmeticNumber);
+
+    while(true){
+        let userNumber = Number(prompt('Угадайте случайное число от 1 до 100'));
+        if (userNumber === 0) {
+            return; 
+        }
+
+        if (userNumber > randomNumber) {
+            alert('Ваше число больше загаднного, попробуйте еще');
+        } else if (userNumber < randomNumber) {
+            alert('Ваше число меньше загаданного, попробуйте еще');
+        } else if (userNumber === randomNumber){
+            alert('Поздравляем! Вы угадали! Загаданное число:' + randomNumber);
+            break;
+        } else {
+            alert('Ввели что-то не то, попробуйте снова');
+        }  
+    }
 }
