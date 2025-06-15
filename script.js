c6765dfcbbd12d4e8fe800e846a0635e9a0fe507
Kalininaaw16012002 <kalinina_asya_1601@mail.ru>
2025-06-15 11:10:54 +0300

    Сделала домашнее задание

--- script.js
+++ script.js
@@ -22,24 +22,46 @@ function guessTheNumber() {
 }
 
 function simpleArithmetic() {
-    let simpleArithmeticNumber = Math.ceil(Math.random()*100);
-    console.log(simpleArithmeticNumber);
+    function operationsMathematical() {
+        let operations = ['+', '-', '/', '*'];
+        let randomIndex = Math.floor(Math.random() * (operations.length - 1)); 
+        let result = operations[randomIndex]; 
+        return result;
+    }
 
-    while(true){
-        let userNumber = Number(prompt('Угадайте случайное число от 1 до 100'));
-        if (userNumber === 0) {
-            return; 
-        }
+    function numbersRandom() {
+        let randomNumber = Math.ceil(Math.random()*20);
+        return randomNumber;
+    }
 
-        if (userNumber > randomNumber) {
-            alert('Ваше число больше загаднного, попробуйте еще');
-        } else if (userNumber < randomNumber) {
-            alert('Ваше число меньше загаданного, попробуйте еще');
-        } else if (userNumber === randomNumber){
-            alert('Поздравляем! Вы угадали! Загаданное число:' + randomNumber);
+    let firstNumber = numbersRandom();
+    let operation = operationsMathematical();
+    let secondNumber = numbersRandom();
+    let userAnswer = "";
+    let result = "";
+
+    switch (operation) {
+        case '+':
+            userAnswer = Number(prompt(`Решите задачу, округлив до целого числа: ${firstNumber} + ${secondNumber}`))
+            result = Math.round(firstNumber + secondNumber);
             break;
-        } else {
-            alert('Ввели что-то не то, попробуйте снова');
-        }  
+        case '-':
+            userAnswer = Number(prompt(`Решите задачу, округлив до целого числа: ${firstNumber} - ${secondNumber}`))
+            result = Math.round(firstNumber - secondNumber);
+            break;
+        case '*':
+            userAnswer = Number(prompt(`Решите задачу, округлив до целого числа: ${firstNumber} * ${secondNumber}`))
+            result = Math.round(firstNumber * secondNumber);
+            break;
+        case '/':
+            userAnswer = Number(prompt(`Решите задачу, округлив до целого числа: ${firstNumber} / ${secondNumber}`))
+            result = Math.round(firstNumber / secondNumber);
+            break;     
+    }
+
+    if (result === userAnswer) {
+        alert(`Все верно! Ответ: ${result}`);
+    } else {
+        alert(`Неверно! Ответ: ${result}`);
     }
 }
